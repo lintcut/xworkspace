c51c141509bcb55f9b41ccfbb0adcdf8df8b57c3
Lintcut <lintcut@gmail.com>
2018-11-07 11:37:20 -0800

    update

--- .xws/xinclude/xbase/xbasedef.h
+++ .xws/xinclude/xbase/xbasedef.h
@@ -30,3 +30,29 @@
 
 #define xmin(a, b)      ((a) < (b) ? (a) : (b))
 #define xmax(a, b)      ((a) > (b) ? (a) : (b))
+
+#ifndef XADDTOPTR
+#define XADDTOPTR(P,I)  ((void*)((unsigned char*)(P) + (I)))
+#endif
+
+#ifndef XROUNDTOSIZE
+#define XROUNDTOSIZE(_address, _alignment)    \
+            (((uintptr_t)(_address) + ((_alignment)-1)) & ~((_alignment) - 1))
+#endif
+
+#ifndef XROUNDTOPAGEBASE
+#define XROUNDTOPAGEBASE(_address, _alignment)    \
+            (((uintptr_t)(_address)) & ~((_alignment) - 1))
+#endif
+
+#ifndef XFLAGON
+#define XFLAGON(_F,_SF)         ((_F) & (_SF))
+#endif
+
+#ifndef XFLAGALLON
+#define XFLAGALLON(_F,_SF)      ((_SF) == ((_F) & (_SF)))
+#endif
+
+#ifndef XFLAGCLEAR
+#define XFLAGCLEAR(_F,_SF)      ((_F) & (~(_SF)))
+#endif
