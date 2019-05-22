bc6fa28d62c32d59a6b10f5626406f52660aa6bb
Lintcut <lintcut@gmail.com>
2019-05-22 10:43:54 -0700

    Rename dir

--- .xws/bin/xws-init.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-
-echo '================================'
-echo '    XWORKSPACE - by Xiang Ye'
-echo '        Version 1.0'
-echo '================================'
-echo '  '
-echo 'Initializing ...'
